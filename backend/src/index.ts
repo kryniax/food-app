bcf3eea07c6e1a0e249ddf78d51ae92b4b5b034c
kryniax <kryniax@gmail.com>
2024-10-31 11:03:57 +0100

    add restaurant endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,29 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import { v2 as cloudinary } from 'cloudinary'
 import myUserRoute from './routes/MyUserRoute';
+import myRestaurantRoute from './routes/MyRestaurantRoute';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(() => console.log("Connected to database"));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", async (req: Request, res: Response) => {
+    res.send({ message: "health ok"});
+})
+
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(7000, () => {
     console.log("server started on localhost:7000")
