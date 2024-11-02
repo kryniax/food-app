50655a2c1e46d75e401fa2431576c098e1e49eac
kryniax <kryniax@gmail.com>
2024-11-02 19:51:02 +0100

    fix redirect bug, add loader

--- frontend/src/auth/ProtectedRoute.tsx
+++ frontend/src/auth/ProtectedRoute.tsx
@@ -1,14 +1,28 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
+import PulseLoader from 'react-spinners/PulseLoader'
 
 const ProtectedRoute = () => {
 
-    const { isAuthenticated } = useAuth0();
+    const { isAuthenticated, isLoading } = useAuth0();
 
-  return (
-    isAuthenticated ? (<Outlet/>) : (<Navigate to="/" replace/>)
-  )
+    if(isLoading) {
+      return (
+        <div className='flex items-center justify-center h-screen'>
+          <PulseLoader 
+            color='#f97316'
+            size={25}
+          />
+        </div>
+      );
+    }
+
+    if(isAuthenticated) {
+      return <Outlet/>
+    }
+  
+    return <Navigate to="/" replace/>
 }
 
 export default ProtectedRoute
