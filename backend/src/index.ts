0d223c13c1179ad018c19926994a231741ae17e8
kryniax <kryniax@gmail.com>
2024-11-04 12:59:46 +0100

    search api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import { v2 as cloudinary } from 'cloudinary'
 import myUserRoute from './routes/MyUserRoute';
 import myRestaurantRoute from './routes/MyRestaurantRoute';
+import restaurantRoute from './routes/RestaurantRoute'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(() => console.log("Connected to database"));
@@ -25,6 +26,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute)
 
 app.listen(7000, () => {
     console.log("server started on localhost:7000")
