c78e99ce1abce059c9858d185857c142287723aa
kryniax <kryniax@gmail.com>
2024-11-06 10:09:12 +0100

    add detail page api

--- backend/src/controllers/RestaurantController.ts
+++ backend/src/controllers/RestaurantController.ts
@@ -1,6 +1,22 @@
 import { Request, Response } from 'express';
 import Restaurant from '../models/resutarant';
 
+const getRestaurant = async (req: Request, res: Response) => {
+    try {
+        const RestaurantId = req.params.restaurantId;
+
+        const restaurant = await Restaurant.findById(RestaurantId);
+        if(!restaurant) {
+            return res.status(404).json({ message: "restaurant not found" });
+        }
+
+        res.json(restaurant);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "something went wrong" });
+    }
+}
+
 const searchRestaurant = async (req: Request, res: Response) => {
     try {
         const city = req.params.city;
@@ -67,5 +83,6 @@ const searchRestaurant = async (req: Request, res: Response) => {
 }
 
 export default {
-    searchRestaurant
+    searchRestaurant,
+    getRestaurant
 }
