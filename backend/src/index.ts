b231e399359ca8214db6812bb6e40bc5a57f99d1
kryniax <kryniax@gmail.com>
2024-11-08 14:42:39 +0100

    add stripe api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from 'cloudinary'
 import myUserRoute from './routes/MyUserRoute';
 import myRestaurantRoute from './routes/MyRestaurantRoute';
 import restaurantRoute from './routes/RestaurantRoute'
+import orderRoute from './routes/OrderRoute'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(() => console.log("Connected to database"));
@@ -26,7 +27,8 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
-app.use("/api/restaurant", restaurantRoute)
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute)
 
 app.listen(7000, () => {
     console.log("server started on localhost:7000")
--- backend/src/models/resutarant.ts
+++ backend/src/models/resutarant.ts
@@ -1,7 +1,12 @@
 import { request } from "http";
-import mongoose from "mongoose";
+import mongoose, { InferSchemaType } from "mongoose";
 
 const menuItemSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        default: () => new mongoose.Types.ObjectId(),
+    },
     name: {
         type: String,
         required: true,
@@ -10,7 +15,9 @@ const menuItemSchema = new mongoose.Schema({
         type: Number,
         required: true,
     }
-})
+});
+
+export type MenuItemType = InferSchemaType<typeof menuItemSchema>;
 
 const restaurantSchema = new mongoose.Schema({
     user: {
